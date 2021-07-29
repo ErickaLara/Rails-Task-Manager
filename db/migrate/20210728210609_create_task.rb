cc3f25488ab60ce8afd87b88ee85bd973b1b761f
ericka <ericka.maricel@gmail.com>
2021-07-29 16:03:28 -0400

    Ejercicio al fin terminado

---
+++ db/migrate/20210728210609_create_task.rb
@@ -0,0 +1,11 @@
+class CreateTask < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
